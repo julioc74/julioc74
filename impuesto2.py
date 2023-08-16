74fd3b0574939a1e3a21416e1b0704a02245beae
julioc74 <cesarng74@gmail.com>
2023-08-16 21:07:00 +0900

    segundo commit

--- impuesto2.py
+++ impuesto2.py
@@ -10,7 +10,13 @@ def preciofinal1(*articulos, imp=15):
  dados como parametros y al no escribirse el valor de la tasa de impuesto se sobreentiende
  que es imp = 15"""    
 preciofinal1(100, 250, 150, 400)
+"""(La salida en pantalla es): El total a pagar es 900
+   La tasa de impuesto es 15.0% y el impuesto a pagar es 135.0
+"""
 
 """se usa una tupla para empaquetar los valores dados como parametros, pero ademas el valor por 
  defecto de la tasa de impuesto cambia a imp = 20"""
 preciofinal1(100, 250, 150, 400, imp=20)
+"""(La salida en pantalla es): El total a pagar es 900
+   La tasa de impuesto es 20.0% y el impuesto a pagar es 180.0
+"""
